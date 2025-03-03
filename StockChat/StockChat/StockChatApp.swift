657662045c16911513de2014ced0178fe6687a2f
duongcuong4395 <dangduongcuog@gmail.com>
2025-03-03 08:37:07 +0700

    init

---
+++ StockChat/StockChat/StockChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockChatApp.swift
+//  StockChat
+//
+//  Created by Macbook on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct StockChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
